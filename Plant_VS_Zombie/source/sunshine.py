3ccb1e0aa77f9f37c9b62f3165b01e3d55c89d45
Frank-Li-Yixuan <3252182195@qq.com>
2024-07-03 23:32:39 +0800

    Delete Plant_VS_Zombie directory

--- Plant_VS_Zombie/source/sunshine.py
+++
@@ -1,8 +0,0 @@
-import objectBase
-class Sunshine(objectBase.ObjectBase):
-    # def checkPosition(self):
-    #     b = super(Sunshine, self).checkPosition()
-    #     if b:
-    #         self.pos[1] += 1
-    #     return b
-    pass
